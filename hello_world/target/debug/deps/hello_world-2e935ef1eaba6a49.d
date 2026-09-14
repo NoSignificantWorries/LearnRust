02a8ce4142704a8f73cb24cf3e2892f5c4d46406
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2026-09-14 21:10:49 +0700

    created simple cargo project

---
+++ hello_world/target/debug/deps/hello_world-2e935ef1eaba6a49.d
@@ -0,0 +1,5 @@
+/home/dmitry/Projects/LearnRust/hello_world/target/debug/deps/hello_world-2e935ef1eaba6a49.d: src/main.rs
+
+/home/dmitry/Projects/LearnRust/hello_world/target/debug/deps/hello_world-2e935ef1eaba6a49: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/dmitry/Projects/LearnRust/hello_world/target/debug/hello_world: /home/dmitry/Projects/LearnRust/hello_world/src/main.rs
