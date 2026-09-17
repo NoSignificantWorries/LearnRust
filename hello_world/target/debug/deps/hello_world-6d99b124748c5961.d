287d074031e42e1d1e0fc6469be71e2af1b886ae
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2026-09-17 15:50:23 +0700

    deleted target in the hello_world project

--- hello_world/target/debug/deps/hello_world-6d99b124748c5961.d
+++
@@ -1,5 +0,0 @@
-/home/dmitry/Projects/LearnRust/hello_world/target/debug/deps/hello_world-6d99b124748c5961.d: src/main.rs
-
-/home/dmitry/Projects/LearnRust/hello_world/target/debug/deps/libhello_world-6d99b124748c5961.rmeta: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-e5f52790d6d80b2c.d
+++
@@ -1,5 +0,0 @@
-/home/dmitry/Projects/LearnRust/hello_world/target/debug/deps/hello_world-e5f52790d6d80b2c.d: src/main.rs
-
-/home/dmitry/Projects/LearnRust/hello_world/target/debug/deps/libhello_world-e5f52790d6d80b2c.rmeta: src/main.rs
-
-src/main.rs:
